7f1754177047e9de9fa6f41e70720e037cf5644e
Samar_Singh_Randhawa <samarsinghrandhawa11@gmail.com>
2023-03-31 12:23:38 +0530

    No

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -65,11 +65,27 @@ export default function Register() {
             // window.sessionStorage.setItem('profileData', JSON.stringify(res.data));
             // navigate('/');
         })
-        .catch((err)=>{
+        .catch((error)=>{
             console.log("Hello1")
             console.log(profdata)
-            console.log(err)
-            console.log(err.request)
+            // console.log(err)
+            // console.log(err.request)
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+              } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+              } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+              }
+              console.log(error.config);
         })
     }
 
